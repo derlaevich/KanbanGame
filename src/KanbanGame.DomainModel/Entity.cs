a78537b6f6b003becb3ae85872e190253198fa0c
Alexsandr Semeov <an.semionov@gmail.com>
2018-08-29 17:39:34 +0300

    created project, added Entity

---
+++ src/KanbanGame.DomainModel/Entity.cs
@@ -0,0 +1,34 @@
+﻿
+namespace KanbanGame.DomainModel
+{
+    public class Entity<T>
+    {
+        public T Id { get; private set; }
+
+        public Entity()
+        {
+            Id = default(T);
+        }
+
+        public Entity(T id)
+        {
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public bool Equals(Entity<T> obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            if (base.Equals(obj))
+                return true;
+
+            return Id.Equals(obj.Id);
+        }
+    }
+}
---
+++ test/KanbanGame.DomainModel.Tests/EntityShould.cs
@@ -0,0 +1,59 @@
+using Xunit;
+
+namespace KanbanGame.DomainModel.Tests
+{
+    public class EntityShould
+    {
+        [Fact]
+        public void CreateInstanceWhithDefaultId_WhenUsingDefaultConstructor()
+        {
+            var entity = new Entity<int>();
+
+            Assert.Equal(default(int), entity.Id);
+        }
+
+        [Fact]
+        public void CreateInstanceWithSpecificId_WhenUsingConstructor()
+        {
+            var entity = new Entity<int>(1);
+
+            Assert.Equal(1, entity.Id);
+        }
+
+        [Fact]
+        public void ÑalculateSameHashCode_IfInstancesHaveIdenticalId()
+        {
+            var entityFirst = new Entity<int>(1);
+            var entitySecond = new Entity<int>(1);
+
+            var hashCodeFirst = entityFirst.GetHashCode();
+            var hashCodeSecond =  entitySecond.GetHashCode();
+
+            Assert.Equal(hashCodeFirst, hashCodeSecond);
+        }
+
+        [Fact]
+        public void ÑalculateDifferentHashCode_IfInstancesHaveDifferentId()
+        {
+            var entityFirst = new Entity<int>(1);
+            var entitySecond = new Entity<int>(2);
+
+            var hashCodeFirst = entityFirst.GetHashCode();
+            var hashCodeSecond = entitySecond.GetHashCode();
+
+            Assert.NotEqual(hashCodeFirst, hashCodeSecond);
+        }
+
+        [Fact]
+        public void EqualWithDifferentInstance_IfEntitiesHaveIdenticalId()
+        {
+            var entityFirst = new Entity<int>(1);
+            var entitySecond = new Entity<int>(1);
+
+            var areEqual = entityFirst.Equals(entitySecond);
+
+            Assert.True(areEqual);
+        }
+    }
+}
+
